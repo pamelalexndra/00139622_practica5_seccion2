9368bd6b6fc1890f20c0928c99b42e6b49ef3a1c
Pamelalexandragomeztenorio <00139622@uca.edu.sv>
2025-10-01 22:41:37 -0600

    feat: Agregar ejercicios base

---
+++ EjercicioFinal/ejercicioFinal/src/Data/scientist.js
@@ -0,0 +1,19 @@
+import cena from '../assets/cena.jpg';
+import roman from '../assets/roman.jpg';
+
+export const scientists = [
+  {
+    name: "John Cena",
+    image: cena,
+    profession: "luchador profesional y actor",
+    awards: "25 (16 veces Campeón Mundial, 5 veces Campeón de Estados Unidos, 4 veces Campeón en Parejas)",
+    discoveries: "motivó a millones con su lema 'Never Give Up' y su trabajo con Make-A-Wish Foundation"
+  },
+  {
+    name: "Roman Reigns",
+    image: roman,
+    profession: "luchador profesional y ex jugador de fútbol americano",
+    awards: "10 (Campeón Universal Indiscutido, Campeón Mundial Peso Pesado, Royal Rumble 2015)",
+    discoveries: "estableció el reinado más largo como campeón universal en la era moderna de WWE"
+  }
+];
